4af834c03d46b1f93e0e0e5580708974b0bc5381
lightlana <lebendaufdensternen@gmail.com>
2022-10-26 23:40:29 +0300

    Задача 58

---
+++ Задача 58/Задача 58.cs
@@ -0,0 +1,60 @@
+﻿// Напишите программу, которая заполнит спирально массив 4 на 4.
+void Task58() 
+{ 
+    int size = 4; 
+    int[,] numbers = new int[size, size]; 
+
+    int index = 0; 
+
+    int currentRow = 0;
+    int currentColumn = 0;
+
+    int indexRow = 0;
+    int indexCol = 1;
+
+    int steps = size;
+    int turn = 0;
+
+    while (index < numbers.Length)
+    {
+        numbers[currentRow, currentColumn] = index + 1;
+        
+        index++;
+        steps--;
+        if (steps == 0)
+        {
+            steps = size - 1 - turn/2;
+            // steps = rows * ((turn+1)%2) + columns * (turn%2) - 1 turn / 2;
+            int temp  = indexRow;
+            indexRow = indexCol;
+            indexCol = -temp;
+            turn++;
+        }
+
+        currentRow += indexRow;
+        currentColumn += indexCol;
+
+    }
+
+    PrintArray(numbers);
+
+} 
+
+
+void PrintArray (int[,] numbers)
+{
+    int rows =  numbers.GetLength(0); // строки 
+    int columns = numbers.GetLength(1); // колонки
+
+    Console.WriteLine("Задан массив:"); 
+    for (int i = 0; i < rows; i++) 
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(numbers[i, j] + "\t"); 
+        } 
+        Console.WriteLine(); 
+    }
+}
+
+Task58();
